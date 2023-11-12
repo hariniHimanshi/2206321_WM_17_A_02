28983c1d1a28bdeab2e3aea4145d94ceca505306
hariniHimanshi <gunarathnehmhh.21@itfac.mrt.ac.lk>
2023-11-11 17:52:29 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const dice = document.getElementById('dice-btn');
+
+function dicegame(){
+    //dice one
+    var random1 = Math.floor((Math.random()*6)+1);
+    var location1 = "png/"+"dice-six-faces-"+random1+".png";
+    var png1 = document.querySelectorAll("img")[0];
+    png1.setAttribute("src",location1);
+    //dice two
+    var random2 =Math.floor((Math.random()*6)+1);
+    var location2 = "png/"+"dice-six-faces-"+random2+".png";
+    var png2 = document.querySelectorAll("img")[1];
+    png2.setAttribute("src",location2);
+
+    if(random1 > random2){
+        document.querySelector("h1").innerHTML = "Player 1 Wins";
+    }
+    else if(random1 < random2){
+        document.querySelector("h1").innerHTML = "Player 2 Wins";
+    }
+    else if(random1 === random2){
+        document.querySelector("h1").innerHTML = "Draw";
+    }
+    else{
+        document.querySelector("h1").innerHTML = "Error";
+    }
+    dice.innerHTML = "Play Again"
+}
